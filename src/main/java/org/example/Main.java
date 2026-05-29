e5bc26db34f3bc24c448a11ed131510e7ad4b98d
vtechthedev <victorfemi9145@gmail.com>
2026-05-29 23:12:05 +0100

    fixed the hashmap and made it static

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,69 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.HashMap;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+
+    static void main() {
+        int attempts = 0;
+        while (true) {
+            attempts++;
+            if (attempts > 2) {
+                System.out.println("the two files have been read successfully ");
+                break;
+            } else {
+                readFiles();
+            }
         }
     }
+
+    public static void readFiles() {
+        System.out.print("which file do you want to read?: ");
+        String choice = scanner.nextLine().strip().toLowerCase();
+        String directory = "src/main/java/org/example/";
+        directory += choice;
+
+//        String choice = "src/main/java/org/example/config_file_development";
+        try (
+                FileReader file = new FileReader(directory);
+                BufferedReader configFile = new BufferedReader(file);
+        ) {
+            HashMap<String, StringBuilder> storedFiles = new HashMap<>();
+            String reader;
+            StringBuilder thewords = new StringBuilder();
+            while ((reader = configFile.readLine()) != null) {
+                thewords.append(reader);
+                thewords.append("\n");
+                System.out.println(reader);
+            }
+            if (choice.equals("config_file_development")) {
+                storedFiles.put("development", thewords);
+            } else if (choice.equals("config_file_production")) {
+                storedFiles.put("production", thewords);
+            }
+            System.out.println("enter \"production\" to get the production files ");
+            System.out.println("enter \"development\" to get the development  files ");
+
+            System.out.print("choose your choice: ");
+            String getFiles = scanner.nextLine().strip().toLowerCase();
+            if (getFiles.equals("production")) {
+                System.out.println(storedFiles.get("production"));
+            } else if (getFiles.equals("development")) {
+                System.out.println(storedFiles.get("development"));
+            } else {
+                System.out.println("invalid input operation has been cancelled");
+            }
+
+        } catch (Exception e) {
+            System.out.println("access denied cannot read the file " + e.getMessage());
+        }
+    }
+
 }
